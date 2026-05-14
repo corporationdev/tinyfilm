40fc27dedcc8e7ed2f3005933ad5510763123a37
Isaac Dyor <isaac@dyor.com>
2026-05-14 08:04:16 -0700

    shadcn

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,34 +1,11 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+import { Button } from '@/components/ui/button'
 
 function App(): React.JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
-
   return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
+    <main className="flex min-h-svh flex-col items-center justify-center gap-4 p-6 text-center">
+      <h1 className="text-2xl font-semibold tracking-normal">shadcn/ui is ready</h1>
+      <Button>Click me</Button>
+    </main>
   )
 }
 
